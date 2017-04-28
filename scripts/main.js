7e8c0f1736cd12fb57cd5cefb9dfd878a30e820b
abdo20 <abdelrhmanadel93@gmail.com>
2017-04-28 03:29:09 +0200

    the project is completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 
 
 $(document).ready(function(){
-	makeGrid('.contianer',16);
+	makeGrid(16);
 	colorOnHover();
 	clearButton();
 });
 
-function makeGrid(contianer,squaresNumber){
+function makeGrid(squaresNumber){
 	for (i = 0; i < squaresNumber; i++) {
    	 for (j = 0; j < squaresNumber; j++){
-     	   $(contianer).append('<div class="square"></div>');
+     	   $('.grid').append('<div class="square"></div>');
      		}
 	}
 }
@@ -27,7 +27,7 @@ function clearButton(){
 		var squaresNumber = prompt("What is the number of squaresXlines you need in the new grid?");
 		var squareWidth = 800/squaresNumber;
 		var squareHeight = 600/squaresNumber;
-		makeGrid('.contianer',squaresNumber);
+		makeGrid(squaresNumber);
 		$('.square').css({'width':squareWidth+'px', 'height':squareHeight + 'px'});
 		colorOnHover();
 	});
