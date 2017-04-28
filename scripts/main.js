686ff90b4a1483da32b5921f03b04ab2dae75292
abdo20 <abdelrhmanadel93@gmail.com>
2017-04-28 03:15:02 +0200

    fix the square size bug

--- scripts/main.js
+++ scripts/main.js
@@ -7,14 +7,10 @@ $(document).ready(function(){
 });
 
 function makeGrid(contianer,squaresNumber){
-	for (i = 0; i < 16; i++) {
-	    var row = '<div class="line">';
-
+	for (i = 0; i < squaresNumber; i++) {
    	 for (j = 0; j < squaresNumber; j++){
-     	   row += '<div class="square"></div>';
+     	   $(contianer).append('<div class="square"></div>');
      		}
-     row += '</div>';
-   	 $(contianer).append(row);
 	}
 }
 
@@ -27,10 +23,12 @@ function colorOnHover(){
 
 function clearButton(){
 	$('button').on('click',function(){
-		$('.contianer').remove();
-		var squaresNumber = prompt("What is the number of squares you need in the new grid?");
-		$('.main').append('<div class="contianer"></div>');
+		$('.square').remove();
+		var squaresNumber = prompt("What is the number of squaresXlines you need in the new grid?");
+		var squareWidth = 800/squaresNumber;
+		var squareHeight = 600/squaresNumber;
 		makeGrid('.contianer',squaresNumber);
+		$('.square').css({'width':squareWidth+'px', 'height':squareHeight + 'px'});
 		colorOnHover();
 	});
 }
